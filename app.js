12333ae5f31b825377d6c7737bf5432266047613
rayan nahi <rayan.nahi99@gmail.com>
2021-08-17 18:31:39 +0200

    MAJ et augmentation de la sécurité

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const dotenv = require('dotenv').config();
+const sanitize = require('express-mongo-sanitize');
+const xssclean = require('xss-clean');
+const helmet = require('helmet');
 
 
 const userRoutes = require('./routes/user');
-const sauceRoutes = require('./routes/sauce')
+const sauceRoutes = require('./routes/sauce');
 const MONGODB_URI = process.env.MONGODB_URI;
+
+
+
 mongoose.connect(MONGODB_URI,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
@@ -15,6 +21,7 @@ mongoose.connect(MONGODB_URI,
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
+app.use(helmet());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -22,6 +29,8 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json());
+app.use(sanitize());
+app.use(xssclean());
 app.use('/images', express.static(path.join(__dirname,'images')));
 
 app.use('/api/auth', userRoutes);
