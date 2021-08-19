6a78287869967f3193c260b2fce4b93a2e723512
rayan nahi <rayan.nahi99@gmail.com>
2021-08-19 22:43:09 +0200

    ajout de la suppression d'images lors de la modification de l'image

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -27,16 +27,28 @@ exports.createSauce = (req, res, next) => {
 
 }
 exports.modifySauce = (req, res, next) => {
+    if (req.file){
+        Sauce.findOne({_id : req.params.id})
+        .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () =>{
+    
+        });
+    })
+    .catch(error => res.status(500).json({ error }))
+    }
     const sauceObject = req.file ? 
-    {
+    {  
         ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : {...req.body}
+    }
+     : {...req.body}
     Sauce.updateOne( {_id : req.params.id}, {...sauceObject, _id : req.params.id})
     .then(() => res.status(200).json({ message: 'Sauce modifiée'}))
     .catch(error => res.status(400).json({error}));
 
 }
+
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({_id : req.params.id})
     .then(sauce => {
